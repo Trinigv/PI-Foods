b7a9b367a3847c646e0fbe807bb22cc187e7c9d1
Trinigv <99trini@gmail.com>
2023-03-14 17:41:29 -0300

    conn string and link and api endpoint

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -1,86 +1,86 @@
-import axios from 'axios'; 
+import axios from 'axios';
 export const GET_RECIPES = 'GET_RECIPES';
 export const GET_DIETS = 'GET_DIETS';
-export const RECIPE_DETAIL = 'GET_RECIPE_DETAIL'; 
-export const CREATE_RECIPE = 'CREATE_RECIPE'; 
+export const RECIPE_DETAIL = 'GET_RECIPE_DETAIL';
+export const CREATE_RECIPE = 'CREATE_RECIPE';
 export const FILTER_BY_DIET = 'FILTER_BY_DIET';
 export const SORT_HEALTHSCORE = 'SORT_HEALTHSCORE';
-export const ALPHABET = 'ALPHABET'
-export const GET_RECIPE_NAME = 'GET_RECIPE_NAME'
+export const ALPHABET = 'ALPHABET';
+export const GET_RECIPE_NAME = 'GET_RECIPE_NAME';
+require('dotenv').config();
+const { API_ENDPOINT } = process.env;
 
 export const getBackendRecipes = () => {
-   return async function ask(dispatch) {
-    let info = await axios.get('http://localhost:3001/recipes')
-    return dispatch({
-        type: GET_RECIPES,
-        payload: info.data 
-    })
- }
-}
+	return async function ask(dispatch) {
+		let info = await axios.get(`${API_ENDPOINT}/recipes`);
+		return dispatch({
+			type: GET_RECIPES,
+			payload: info.data,
+		});
+	};
+};
 export const getBackendDiets = () => {
-    return async (dispatch) => {
-        let diets = await axios.get('http://localhost:3001/diets')
-        return dispatch({
-            type: GET_DIETS,
-            payload: diets.data
-        })
-    }
-}
-
-export const getRecipesByName = (title) => {
-    return async (dispatch) => {
-        let response = await axios.get(`http://localhost:3001/recipes/?title=${title}`)
-        return dispatch({
-            type: GET_RECIPE_NAME,
-            payload: response.data
-        })
-    }
-}
-
-export const postRecipe = (payload) => {
-    return async (dispatch) => {
-        const res = await axios.post('http://localhost:3001/recipes/create', payload)
-        return {
-            type: CREATE_RECIPE,
-            res
-        }
-    }
-
-}
-export const getRecipeDetail = (payload) => {
-    return async function (dispatch) {
-        const details = await axios.get(`http://localhost:3001/recipes/${payload}`)
-        return dispatch({
-            type: RECIPE_DETAIL,
-            payload: details.data
-        })
-    }
-
-}
-
-export function filterByDiet (payload) {
-    return {
-        type: FILTER_BY_DIET,
-        payload
-    }
+	return async (dispatch) => {
+		let diets = await axios.get(`${API_ENDPOINT}/diets`);
+		return dispatch({
+			type: GET_DIETS,
+			payload: diets.data,
+		});
+	};
 };
 
+export const getRecipesByName = (title) => {
+	return async (dispatch) => {
+		let response = await axios.get(`${API_ENDPOINT}?title=${title}`);
+		return dispatch({
+			type: GET_RECIPE_NAME,
+			payload: response.data,
+		});
+	};
+};
+
+export const postRecipe = (payload) => {
+	return async (dispatch) => {
+		const res = await axios.post(`${API_ENDPOINT}/recipes/create`, payload);
+		return {
+			type: CREATE_RECIPE,
+			res,
+		};
+	};
+};
+export const getRecipeDetail = (payload) => {
+	return async function (dispatch) {
+		const details = await axios.get(`${API_ENDPOINT}/recipes/${payload}`);
+		return dispatch({
+			type: RECIPE_DETAIL,
+			payload: details.data,
+		});
+	};
+};
+
+export function filterByDiet(payload) {
+	return {
+		type: FILTER_BY_DIET,
+		payload,
+	};
+}
+
 export function sortByHealthscore(payload) {
-    return {
-        type: SORT_HEALTHSCORE,
-        payload
-    }
+	return {
+		type: SORT_HEALTHSCORE,
+		payload,
+	};
 }
 
 export function sortAlphabetically(payload) {
-    return {
-        type: ALPHABET,
-        payload
-    }
+	return {
+		type: ALPHABET,
+		payload,
+	};
 }
 
 export function deleteState() {
-    return {
-        type: 'DELETE',
-    }
+	return {
+		type: 'DELETE',
+	};
 }
